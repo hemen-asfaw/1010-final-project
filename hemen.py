6b75187e2962125167f6edd75df8e3f7319e4fa2
hemen-asfaw <97702280+hemen-asfaw@users.noreply.github.com>
2022-12-11 22:21:19 -0800

    Update hemen.py

--- hemen.py
+++ hemen.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import random
 import sys
+import time
 pygame.init()
 
 screen_x = 1280
@@ -26,44 +27,44 @@ Obstacles = [pygame.image.load(os.path.join("assets", "kiki_bots.PNG")),
                 pygame.image.load(os.path.join("assets", "obstacle_2.PNG"))]
 Grass = pygame.image.load(os.path.join("assets", "grass.jpg"))
 
+
+
+
 class Iggy():
     #we're setting iggy at a stationary position because it does not change its position
-    x = 250
-    y = 510 
-    gravity = 8.5 
+    # x = 250
+    # y = 510 
+    GRAVITY = 10
     
     def __init__(self):
-        self.iggy_run = True 
         self.iggy_jump = False 
         self.run_img = Running
-        self.jump_img = Jumping
-    
-        self.Gravity = self.gravity
+        #self.jump_img = Jumping
+        self.y = 250
+        self.x = 510
+        self.gravity = self.GRAVITY 
         self.current_image = 0 
         self.image = self.run_img[0]
         self.rect = self.image.get_rect(center=(self.x, self.y))
 
     #In the update funcion we will put in the different actions of iggy that will change as game goes on 
     def update(self, inputs):
-        #keys = pygame.key.get_pressed()
 
-        if self.iggy_run:
-            self.run()
+        if inputs.type == pygame.KEYDOWN:
+            if inputs.key == pygame.K_UP or inputs.key == pygame.K_SPACE:
+                self.iggy_jump = True
+
         if self.iggy_jump:
             self.jump()
+        else:
+            self.y = 510
+            self.x = 250
+            self.run()
         # if self.current_image >= 10:
         #     self.step_index = 0
-        
-        if inputs == pygame.K_UP and not self.iggy_jump:
-            self.iggy_run = False 
-            self.iggy_jump = True  
 
-        elif inputs == pygame.K_SPACE and not self.iggy_jump:
-            self.iggy_run = False 
-            self.iggy_jump = True
-        else: 
-            self.iggy_run = True
-            self.iggy_jump = False
+        self.rect.x = self.x
+        self.rect.y = self.y
     #come back to this if problem with iggy appearing
     def run(self):
         self.current_image += 0.08 
@@ -76,34 +77,85 @@ class Iggy():
         screen.blit(self.image, self.rect)
 
     def jump(self):
-        self.image = self.jump_img
+        #self.image = self.jump_img
         if self.iggy_jump:
-            self.iggy_jump.y -= self.gravity *4
-            self.gravity -= 0.8
-        if self.gravity < -(self.Gravity):
+            self.y -= self.gravity * 4
+            self.gravity -= 0.5 # change speed of the jump
+        if self.gravity < -(self.GRAVITY):
             self.iggy_jump = False
-            self.Gravity = self.gravity
+            self.gravity = self.GRAVITY
+    
+
+
+
+    # def jump(self):
+    #     #self.image = self.jump_img
+    #     #time.sleep(1)
+    #     self.y -= self.gravity *4
+    #     self.gravity -= 0.8
+    #     #self.iggy_run = False
+    #     print(self.y)
+    #     if self.y == 505:
+    #         self.y += self.gravity *4
+    #         self.gravity += 0.8
+    #         if self.y == 510:
+    #             self.iggy_run = False
+
+
+        # self.y = 800
+        # print(self.y)
+        # time.sleep(3)
+        # self.y = 510
+        # #time.sleep(1)
+        # self.iggy_run = True
+        # self.image = self.jump_img
+        # print(self.y)
+        # self.y -= self.gravity *4
+        # self.gravity -= 0.8
+        
+        # if self.y == 505:
+        #     print("FIRST") 
+        #     self.iggy_jump = False
         
 
+        #     #self.gravity < -self.Gravity
+        #     self.y += self.gravity *10
+        #     self.gravity += 0.8
+        #     self.y = 510
+        #     #self.iggy_jump = False
+        #     # self.Gravity = self.gravity
+        #     print("come down")
 
-def main_function():
-    running = True 
+        #     # if self.y == 510:
+        #     #     self.iggy_jump = True
+
+
+            
+
+def main_function(): 
     player = Iggy()
 
     #in the while loop we will have all the things that should be runnning 
     #endlessly throughout the game  
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print("quitting")
                 pygame.quit()
                 sys.exit() 
         
-        screen.fill("light blue")
-        player.display()
         player_input = pygame.key.get_pressed()
 
-        player.update(player_input)
+        if player_input[pygame.K_SPACE]:
+            print("pressed")
+            player.iggy_jump = True 
+
+        print(player.y)
+        print(player.iggy_jump)
+
+        screen.fill("light blue")
+        player.display()
+        player.update(event)
         pygame.display.update()
 
 
